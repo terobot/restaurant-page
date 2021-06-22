2f2bbbb8729c9185c54edcaacd93e0f21af735b3
terobot <tero.t.kononen@gmail.com>
2021-06-23 00:29:23 +0300

    Update styles, add about text

--- src/home.js
+++ src/home.js
@@ -1,7 +1,18 @@
 const home = () => {
     const homeEl = document.createElement('div')
-    homeEl.innerHTML = 'Placeholder text for HOME'
+    const aboutEl = document.createElement('div')
+    const aboutHeaderEl = document.createElement('h1')
+    const aboutTextEl = document.createElement('p')
+    const historyHeaderEl = document.createElement('h1')
+    const historyTextEl = document.createElement('p')
+    aboutEl.classList.add('about')
+    aboutHeaderEl.innerHTML = 'About us'
+    aboutTextEl.innerHTML = 'Good food for hungry people. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    historyHeaderEl.innerHTML = 'History'
+    historyTextEl.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    aboutEl.append(aboutHeaderEl, aboutTextEl, historyHeaderEl, historyTextEl)
     homeEl.classList.add('home')
+    homeEl.append(aboutEl)
 
     return homeEl
 }
