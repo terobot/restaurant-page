2de354918999b5d283d6a5a013001fcf48c28dbc
terobot <tero.t.kononen@gmail.com>
2021-07-10 23:48:58 +0300

    Update menu content

--- src/footer.js
+++ src/footer.js
@@ -1,8 +1,13 @@
 import './style.css'
 
 const footer = () => {
+    const attribution = 'Background photo by <a href="https://unsplash.com/@amseaman?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Andrew Seaman</a> on <a href="https://unsplash.com/s/photos/restaurant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>'
     const footerEl = document.createElement('div')
-    footerEl.innerHTML = 'Street 123, City, Country'
+    const footerTextEl = document.createElement('p')
+    const attributionEl = document.createElement('p')
+    footerTextEl.innerHTML = 'Street 123, City, Country'
+    attributionEl.innerHTML = attribution
+    footerEl.append(footerTextEl, attributionEl)
     footerEl.classList.add('footer')
 
     return footerEl
--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,7 @@
 import item from './item'
 import standardBurgerSrc from './assets/junior-reis-K9vGRD_47do-unsplash.jpg'
 import greenBurgerSrc from './assets/junior-reis-XdNgE6NIHp4-unsplash.jpg'
+import chiliBurgerSrc from './assets/junior-reis--Xr6MYJkZ8E-unsplash.jpg'
 
 const menu = () => {
     const attribution = 'Photo by <a href="https://unsplash.com/@juniorreisfoto?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Junior REIS</a> on <a href="https://unsplash.com/s/photos/burger?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>'
@@ -8,8 +9,9 @@ const menu = () => {
     const containerEl = document.createElement('div')
     const standardBurgerEl = item('Standard burger', 'Bun, beef, cheese, pickles and mayo', '12€', standardBurgerSrc, 'Standard burger', attribution)
     const greenBurgerEl =  item('Green burger', 'Bun, vegan patty, cheese, lettuce, onion and mayo', '10€', greenBurgerSrc, 'Green burger', attribution)
+    const chiliBurgerEl =  item('Chili burger', 'Bun, beef, cheese, chili and mayo', '15€', chiliBurgerSrc, 'Chili burger', attribution)
     containerEl.classList.add('container')
-    containerEl.append(standardBurgerEl, greenBurgerEl)
+    containerEl.append(standardBurgerEl, greenBurgerEl, chiliBurgerEl)
     menuEl.classList.add('menu')
     menuEl.append(containerEl)
 
