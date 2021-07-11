ecc07b16385b26b015190cb897f5ff79b95e3981
terobot <tero.t.kononen@gmail.com>
2021-07-11 22:07:21 +0300

    Add sides and drinks to menu

--- src/home.js
+++ src/home.js
@@ -7,9 +7,9 @@ const home = () => {
     const historyTextEl = document.createElement('p')
     containerEl.classList.add('container')
     aboutHeaderEl.innerHTML = 'About us'
-    aboutTextEl.innerHTML = 'Good food for hungry people. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    aboutTextEl.innerHTML = 'Good food for hungry people. At Good Food Inc, we wish to offer good food to you no matter who you are and where you come from with an innovative gastronomic take on traditional cooking methods, fine produce and the legacy of our shared food heritage. Moreover, we regard it as a personal challenge to help bring about a revival of good food and let its distinctive flavours and particular regional character brighten up the world.'
     historyHeaderEl.innerHTML = 'History'
-    historyTextEl.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    historyTextEl.innerHTML = 'Good Food Inc restaurant began on May 14th, 1988 in the city of Eagle Bay. Focusing on the goals of “Creativity, Quality, Healthy, and Tradition” the company is family operation making each dish in our own kitchens from scratch. Good Food Inc has three generations of family member working together to fulfill the idea “Honor the Community, Quality Comes First”. Through creativity and curiousity, Good Food Inc began building its brand around the idea of delicious and authentic good food.'
     containerEl.append(aboutHeaderEl, aboutTextEl, historyHeaderEl, historyTextEl)
     homeEl.classList.add('home')
     homeEl.append(containerEl)
--- src/menu.js
+++ src/menu.js
@@ -2,16 +2,34 @@ import item from './item'
 import standardBurgerSrc from './assets/junior-reis-K9vGRD_47do-unsplash.jpg'
 import greenBurgerSrc from './assets/junior-reis-XdNgE6NIHp4-unsplash.jpg'
 import chiliBurgerSrc from './assets/junior-reis--Xr6MYJkZ8E-unsplash.jpg'
+import friesSrc from './assets/junior-reis-XcpdGpiyj1c-unsplash.jpg'
+import smashedPotatoSrc from './assets/junior-reis-bHIT0h_cqBI-unsplash.jpg'
+import vegsSrc from './assets/junior-reis-MmnR8lxvpEs-unsplash.jpg'
+import beerSrc from './assets/engin-akyurt-3ORoQEJY9LA-unsplash.jpg'
+import colaSrc from './assets/giovanna-gomes-IHKR_A_THW0-unsplash.jpg'
 
 const menu = () => {
     const attribution = 'Photo by <a href="https://unsplash.com/@juniorreisfoto?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Junior REIS</a> on <a href="https://unsplash.com/s/photos/burger?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>'
+    const beerAtt = 'Photo by <a href="https://unsplash.com/@enginakyurt?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">engin akyurt</a> on <a href="https://unsplash.com/s/photos/beer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>'
+    const colaAtt = 'Photo by <a href="https://unsplash.com/@giisilveira?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Giovanna Gomes</a> on <a href="https://unsplash.com/s/photos/soda?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>'
     const menuEl = document.createElement('div')
     const containerEl = document.createElement('div')
+    const mainsHeaderEl = document.createElement('h1')
+    const sidesHeaderEl = document.createElement('h1')
+    const drinksHeaderEl = document.createElement('h1')
     const standardBurgerEl = item('Standard burger', 'Bun, beef, cheese, pickles and mayo', '12€', standardBurgerSrc, 'Standard burger', attribution)
     const greenBurgerEl =  item('Green burger', 'Bun, vegan patty, cheese, lettuce, onion and mayo', '10€', greenBurgerSrc, 'Green burger', attribution)
     const chiliBurgerEl =  item('Chili burger', 'Bun, beef, cheese, chili and mayo', '15€', chiliBurgerSrc, 'Chili burger', attribution)
+    const friesEl =  item('Fries', 'Potato fries, fried to perfection', '3€', friesSrc, 'Fries', attribution)
+    const smashedPotatoEl =  item('Smashed potato', 'Loaded smashed potato', '5€', smashedPotatoSrc, 'Smashed potato', attribution)
+    const vegsEl =  item('Vegs', 'Seasonal vegs fried', '5€', vegsSrc, 'Vegs', attribution)
+    const beerEl =  item('Beer', 'Draft beer', '7€', beerSrc, 'beer', beerAtt)
+    const colaEl =  item('Cola', 'Cola from draft', '3€', colaSrc, 'beer', colaAtt)
+    mainsHeaderEl.innerHTML = 'Mains'
+    sidesHeaderEl.innerHTML = 'Sides'
+    drinksHeaderEl.innerHTML = 'Drinks'
     containerEl.classList.add('container')
-    containerEl.append(standardBurgerEl, greenBurgerEl, chiliBurgerEl)
+    containerEl.append(mainsHeaderEl, standardBurgerEl, greenBurgerEl, chiliBurgerEl, sidesHeaderEl, friesEl, smashedPotatoEl, vegsEl, drinksHeaderEl, beerEl, colaEl)
     menuEl.classList.add('menu')
     menuEl.append(containerEl)
 
